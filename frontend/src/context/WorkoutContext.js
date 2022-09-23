5980739a5e296f2c8ac804b9772d9056c7db6771
Kennychibiam <kenchibiam@gmail.com>
2022-09-23 09:32:53 +0100

    first commit

---
+++ frontend/src/context/WorkoutContext.js
@@ -0,0 +1,36 @@
+import { createContext, useReducer } from "react";
+
+
+export const WorkoutsContext = createContext()
+export const workoutsReducer = (state, action) => {
+    switch (action.type) {
+        case 'SET_WORKOUTS':
+            return { workouts: action.payload }
+        case 'CREATE_WORKOUT':
+            return { workouts: [action.payload, ...state.workouts] }
+        case 'DELETE_WORKOUT':
+            console.log("reached")
+            console.log(action.payload)
+            console.log(state)
+            const temp=state.workouts.filter(work=>work._id!==action.payload.workout._id)
+
+            
+            console.log(temp)
+        return {
+            workouts:temp
+        }
+        default: return state;
+    }
+}
+export const WorkoutsContextProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(workoutsReducer, {
+        workouts: null
+    })
+    return (
+        <WorkoutsContext.Provider value={{ ...state, dispatch }}>
+            {children}
+
+        </WorkoutsContext.Provider>
+    )
+
+}
